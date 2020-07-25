727899dcb47f1bc1b743987b662c4c1b74f2146b
Rudra Makwana <rudrajmakwana@gmail.com>
2020-07-25 01:45:32 -0300

    Doctors Lists related APIs are implemented.

--- controllers/searchDoctorController.js
+++ controllers/searchDoctorController.js
@@ -1,22 +1,51 @@
 const Doctors = require('../models/Doctors');
+const e = require('express');
+const { type } = require('jquery');
+const { request } = require('express');
 
 module.exports = () => {
     exports.getDoctors = (req, res) => {
         
         const {keyword, searchfield} = req.body;
-        
-        if(searchfield == "name"){
-            resulr = Doctors.find({name: new RegExp(keyword, 'i'), type: "doctor"}, function(err, result){
-                if (err) throw err;
-                res.json(result);
-            });
+
+        if (keyword.length == 0 || searchfield == ""){
+            res.sendStatus(403);
         }
-        else{
-            resulr = Doctors.find({specialization: new RegExp(keyword, 'i'), type: "doctor"}, function(err, result){
-                if (err) throw err;
-                res.json(result);
-            });
-        }
+        else{    
+            if(searchfield == "Name"){
+                resulr = Doctors.find({name: new RegExp(keyword, 'i'), type: "doctor"}, function(err, result){
+                    if (err) throw err;
+                    res.json(result);
+                });
+            }
+            else{
+                resulr = Doctors.find({specialization: new RegExp(keyword, 'i'), type: "doctor"}, function(err, result){
+                    if (err) throw err;
+                    res.json(result);
+                });
+            }
+        }    
+    }
+
+    exports.getAllDoctors = (req, res) => {
+        results = Doctors.find({type: "doctor"}, function(err, result){
+            if(err) throw err;
+            res.json(result);
+        })
+    }
+
+    exports.getAllPhysicians = (req, res) => {
+        results = Doctors.find({type: "doctor", specialization: new RegExp("general physician", 'i')}, function(err, result){
+            if(err) throw err;
+            res.json(result);
+        });
+    }
+
+    exports.getAllSurgeons = (req, res) => {
+        results = Doctors.find({type: "doctor", specialization: new RegExp("surgeon", 'i')}, function(err, result){
+            if(err) throw err;
+            res.json(result);
+        });
     }
 
     return exports;
--- routes/search-doctors.js
+++ routes/search-doctors.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const Doctors = require('../models/Doctors');
 
 module.exports = () => {
-    const { getDoctors } = require('../controllers/searchDoctorController')();
+    const { getDoctors, getAllDoctors, getAllPhysicians, getAllSurgeons } = require('../controllers/searchDoctorController')();
     
+    console.log("Route is hit");
     router.post("/search-doctors", getDoctors);
+
+    router.post("/alldoctors",getAllDoctors);
+
+    router.post("/all-physicians", getAllPhysicians);
+
+    router.post("/all-surgeons", getAllSurgeons);
 
     return router;
 }
